319aed59c2d29a54d88efecfbfe0fffcc14d8939
nursahtuncel <nursahtuncell@gmail.com>
2025-08-02 17:32:02 +0300

    [JU-4] - create header section

--- src/components/HeaderComponent/header.js
+++ src/components/HeaderComponent/header.js
@@ -1,13 +1,32 @@
-
-import "./header.css"
-const Header = ()=>{
-   
-
-const section =document.createElement("div");
-section.classList.add("example");
- section.innerHTML=`
-  <h2>Header</h2>
- `
-    return section;
-}
-export default Header ;
+import "./header.css";
+import basketIcon from "./../../../images/icons/basketIcon.svg";
+import userIcon from "./../../../images/icons/userIcon.svg";
+import searchIcon from "./../../../images/icons/searchIcon.svg";
+const Header = () => {
+  const section = document.createElement("div");
+  section.classList.add("example");
+  section.innerHTML = `
+    <div class="header">
+      <div class="headerLogo">SHOP.CO</div>
+      <nav>
+        <ul class="headerUL">
+          <li>Shop</li>
+          <li>On Sale</li>
+          <li>New Arrivals</li>
+          <li>Brands</li>
+        </ul>
+      </nav>
+     <div class="headerSearchContainer">
+     <img class="headerSearchIcon" src="${searchIcon}" alt=""/>
+     <input class="searchInput" type="text" placeholder="search for product..">
+     </div>
+     
+          <div class="headerIcons">
+    <img src="${basketIcon}" alt=""/>
+    <img src="${userIcon}" alt=""/>
+           </div>
+    </div>
+ `;
+  return section;
+};
+export default Header;
